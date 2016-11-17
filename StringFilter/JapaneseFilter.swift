f092a462671e6d1ba4b0b227717547979b5b03f6
tnantoka <tnantoka@bornneet.com>
2016-11-17 21:56:53 +0900

    Fix JapaneseFilter for Linux

--- StringFilter/JapaneseFilter.swift
+++ StringFilter/JapaneseFilter.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreFoundation
 
 public indirect enum JapaneseString {
     case katakana
@@ -35,8 +36,12 @@ extension String {
         }
     }
     
-    func replace(_ pattern: String, replacement: (String) -> String) -> String {
+    func replace(_ pattern: String, replacement: @escaping (String) -> String) -> String {
+#if os(Linux)
+        guard let regex = try? RegularExpression(pattern: pattern, options: []) else { return self }
+#else
         guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else { return self }
+#endif
         let mutableString = NSMutableString(string: self)
         regex.enumerateMatches(in: self, options: [], range: NSMakeRange(0, characters.count)) { result, _, _ in
             guard let result = result else { return }
@@ -44,7 +49,26 @@ extension String {
             let matched = mutableString.substring(with: range)
             mutableString.replaceCharacters(in: range, with: replacement(matched))
         }
-        return mutableString as String
+        return String(describing: mutableString)
+    }
+    
+    func transformFullHalf(_ string: String, reverse: Bool) -> String {
+        let mutableString = NSMutableString(string: string)
+#if os(Linux)
+        let cfMutableString = unsafeBitCast(mutableString, to: CFMutableString.self)
+#else
+        let cfMutableString = mutableString as CFMutableString
+#endif
+        CFStringTransform(cfMutableString, nil, kCFStringTransformFullwidthHalfwidth, reverse)
+        return String(describing: cfMutableString)
+    }
+    
+    var full: String {
+        return transformFullHalf(self, reverse: true)
+    }
+    
+    var half: String {
+        return transformFullHalf(self, reverse: false)
     }
 }
 
@@ -64,26 +88,12 @@ extension JapaneseFilter: StringFilterType {
         case (.full(.number), .half(.number)):
             return string.shift(-Distance.alnum, pattern: "[０-９]")
         case (.half(.katakana), .full(.katakana)):
-            return string.replace("\\p{Katakana}") { full($0) }
+            return string.replace("\\p{Katakana}") { $0.full }
         case (.full(.katakana), .half(.katakana)):
-            return string.replace("\\p{Katakana}") { half($0) }
+            return string.replace("\\p{Katakana}") { $0.half }
         default:
             return string
         }
-    }
-    
-    func transformFullHalf(_ string: String, reverse: Bool) -> String {
-        let mutableString = NSMutableString(string: string)
-        CFStringTransform(mutableString, nil, kCFStringTransformFullwidthHalfwidth, reverse)
-        return mutableString as String
-    }
-    
-    func full(_ string: String) -> String {
-        return transformFullHalf(string, reverse: true)
-    }
-    
-    func half(_ string: String) -> String {
-        return transformFullHalf(string, reverse: false)
     }
 }
 
